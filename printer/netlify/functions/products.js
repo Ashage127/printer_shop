ea70c4e57ac832b1568f64a47ae3c7eb9a924047
Ashage127 <rasmus.raadla@gmail.com>
2026-09-22 23:33:33 +0300

    Update products.js

--- printer/netlify/functions/products.js
+++ printer/netlify/functions/products.js
@@ -6,13 +6,13 @@ const SUPABASE_KEY = process.env.SUPABASE_KEY;
 exports.handler = async function () {
   try {
     const response = await fetch(
-      `${SUPABASE_URL}/rest/v1/products?id=eq.1&select=*`,
-      {
-        headers: {
-          apikey: SUPABASE_KEY
-        }
-      }
-    );
+  `${SUPABASE_URL.replace(/\/$/, "")}/rest/v1/products?id=eq.1&select=*`,
+  {
+    headers: {
+      apikey: SUPABASE_KEY
+    }
+  }
+);
 
     const data = await response.json();
 
