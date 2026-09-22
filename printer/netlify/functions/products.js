3553f74db22f34a411167062b7e3d44a399e2f65
Ashage127 <rasmus.raadla@gmail.com>
2026-09-22 23:24:48 +0300

    Update products.js

--- printer/netlify/functions/products.js
+++ printer/netlify/functions/products.js
@@ -4,8 +4,32 @@ const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_KEY = process.env.SUPABASE_KEY;
 
 exports.handler = async function () {
-  return {
-  statusCode: 200,
-  body: "Backend töötab"
- };
-}; 
+  try {
+    const response = await fetch(
+      `${SUPABASE_URL}/rest/v1/products?id=eq.1&select=*`,
+      {
+        headers: {
+          apikey: SUPABASE_KEY
+        }
+      }
+    );
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message
+      })
+    };
+  }
+};
